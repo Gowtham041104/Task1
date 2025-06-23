375ebac8c90eee424bb64fe7b06664e9bfe5c8ff
unknown <gowtham thamg1848@gmail.com>
2025-06-23 11:58:20 -0700

    mongo db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const connectDb =async()=>{
     try{
-        const conn = await mongoose.connect((process.env.MONGO_URI ||"mongodb://localhost:27017/saasproject"),{
+        const conn = await mongoose.connect((process.env.MONGO_URI),{
             useNewUrlParser:true,
             useUnifiedTopology:true
         });
